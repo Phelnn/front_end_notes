3be5c5ae7ed887f4d989ec5ee85e9e93ecaf9ad7
Phelnn <Phel_Iren@outlook.com>
2023-06-06 08:51:47 +0800

    django project updating

--- lecture7/authentication/users/views.py
+++ lecture7/authentication/users/views.py
@@ -13,5 +13,19 @@ def index(request):
             "user": request.user
     } 
     return render(request, "users/user.html, context")
-1:25:36
 
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))  #Redirect重定向到index函数， reverse的作用是通过函数名反查url，和urls.py中函数名与url路径关联有关
+    else:
+        return render(request, "users/login.html"), {"message": "Invalid credit"}
+    
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message": "Logged out."})
