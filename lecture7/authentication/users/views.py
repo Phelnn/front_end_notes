b5a59551789f184b680550cd9c8dd5825860b732
Phelnn <Phel_Iren@outlook.com>
2023-06-08 20:22:31 +0800

    django project updating

--- lecture7/authentication/users/views.py
+++ lecture7/authentication/users/views.py
@@ -7,23 +7,24 @@ from django.urls import reverse
 
 
 def index(request):
-    if not request.user.is_authenticated:
+    if not request.user.is_authenticated:                               #user是request对象里包含的一个属性（其本身也是对象），可以通过login()、logout()等函数改变状态
         return render(request, "users/login.html", {"message": None})
     context = {
             "user": request.user
     } 
-    return render(request, "users/user.html, context")
+    return render(request, "users/user.html", context)
 
 
 def login_view(request):
     username = request.POST["username"]
     password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
+    user = authenticate(request, username=username, password=password)  #看起来是个能读取用户库的函数，验证用户名和密码是否正确，正确的话返回详细的用户信息，否则返回none
     if user is not None:
         login(request, user)
+        #疑问：为什么不直接调用index函数而是使用下面的方式？因为下面的方式会重新发起http请求并进行连接，直接调用index(request)不会收到回传的HttpResposeObject，浏览器会报错
         return HttpResponseRedirect(reverse("index"))  #Redirect重定向到index函数， reverse的作用是通过函数名反查url，和urls.py中函数名与url路径关联有关
     else:
-        return render(request, "users/login.html"), {"message": "Invalid credit"}
+        return render(request, "users/login.html", {"message": "Invalid credit"})
     
 
 def logout_view(request):
